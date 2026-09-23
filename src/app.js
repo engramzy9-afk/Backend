350b21c86a21a854a697e00973bd6aa4220ca61a
Osama Ramzy <engramzy9@gmail.com>
2026-09-23 11:00:46 +0300

    Add Swagger Editor

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const {
   securityHeaders,
@@ -44,6 +45,19 @@ function createApp() {
   app.use(express.json({ limit: '1mb' }));
 
   app.use(express.urlencoded({ extended: true }));
+
+  app.use('/docs', express.static(
+    path.join(__dirname, '../node_modules/swagger-editor-dist')
+  ));
+
+  app.get('/docs', (req, res) => {
+    res.sendFile(
+      path.join(
+        __dirname,
+        '../node_modules/swagger-editor-dist/index.html'
+      )
+    );
+  });
 
   app.use('/api/v1/auth/login', authLimiter);
 
